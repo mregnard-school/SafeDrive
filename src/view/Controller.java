13fb435e7a74967f1748ebd5726278c620ca48bd
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-11-29 16:39:57 -0500

    bind view, dunno if it works

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,5 +1,30 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+  @FXML
+  TextField iterationsInput;
+
+  @FXML
+  TextField vehiclesInput;
+
+  @FXML
+  Pane simulationPane;
+
+  @FXML
+  AnchorPane logPane;
+
+  @FXML
+  private void runSimulation(ActionEvent event) {
+    System.out.println(this.vehiclesInput.getText());
+    System.out.println(this.iterationsInput.getText());
+    System.out.println("click on run simulation");
+  }
 
 }
