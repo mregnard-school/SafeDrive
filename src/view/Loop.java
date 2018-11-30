cb7a17df1631969d7a8b5f979560dbcfff5aaae1
Irindul <mathieu.regnard2@gmail.com>
2018-11-30 13:47:44 -0500

    Added buffer logic into loop

--- src/view/Loop.java
+++ src/view/Loop.java
@@ -2,19 +2,30 @@ package view;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.function.Consumer;
+import model.agents.Agent;
 import model.environment.Simulation;
 import util.IntentList;
+import util.IntentList.Intent;
 
 public class Loop implements Runnable {
 
   private Simulation simulation;
   private boolean run;
   private PropertyChangeSupport support;
+  private Queue<IntentList> buffer;
+  //private int displayedStep = 0;
 
   public Loop(Simulation simulation) {
     this.simulation = simulation;
     this.run = false;
     support = new PropertyChangeSupport(this);
+    buffer = new LinkedList<>();
   }
 
   public void addPropertyChangeListener(PropertyChangeListener listener) {
@@ -35,7 +46,7 @@ public class Loop implements Runnable {
   }
 
   private boolean shouldRun() {
-    return simulation.hasNext() && run;
+    return (simulation.hasNext() || !buffer.isEmpty()) && run;
   }
 
   @Override
@@ -45,13 +56,29 @@ public class Loop implements Runnable {
       while (shouldRun()) {
         simulation.next();
         long loopTime = System.currentTimeMillis();
+        buffer.add(simulation.getIntents());
+
+        System.out.println("Loop time " + loopTime);
+        System.out.println("Current time " + currentTime);
+        System.out.println("diff " + (loopTime - currentTime));
 
         if (loopTime - currentTime > 1000) {
-          //Draw
+          System.out.println("What");
+
+          draw();
           currentTime = loopTime;
         }
         //support.firePropertyChange("simulation", false, simulation);
       }
+    }
+  }
+
+  private void draw() {
+    IntentList intents = buffer.poll();
+    if (intents != null) {
+      System.out.println("Firering");
+
+      support.firePropertyChange("simulation", this.simulation, intents);
     }
   }
 }
