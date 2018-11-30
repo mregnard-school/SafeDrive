6a1a5a3ae95653c1eaf6e8b210cdae633b9ce216
Irindul <mathieu.regnard2@gmail.com>
2018-11-30 13:48:15 -0500

    Mock up for agent movement

--- src/model/environment/Simulation.java
+++ src/model/environment/Simulation.java
@@ -1,11 +1,18 @@
 package model.environment;
 
+import java.awt.Point;
+import java.util.List;
+import model.agents.Agent;
+import model.communication.Command;
+import util.IntentList;
+import util.IntentList.Intent;
 import util.Logger;
 
 public class Simulation {
 
   private int maxIterations;
   private int currentStep;
+  private IntentList intents; 
 
   public Simulation(int maxIterations) {
     this.maxIterations = maxIterations;
@@ -25,17 +32,30 @@ public class Simulation {
       return;
     }
     this.currentStep++;
+    intents = new IntentList();
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    Agent a  = new Agent() {
+      @Override
+      public List<Command> getCommands() {
+        return null;
+      }
+
+      @Override
+      public void setDirection(Direction direction) {
+
+      }
+    };
+
+    intents.addIntent(a, new Point(currentStep-1, 0), new Point(currentStep, 0));
 
     Logger.getLogger().log("Finished stepped " + currentStep);
   }
 
   public boolean hasNext() {
     return currentStep < maxIterations;
+  }
+
+  public IntentList getIntents() {
+    return this.intents;
   }
 }
