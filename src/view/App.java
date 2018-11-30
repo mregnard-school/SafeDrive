755cb847f29099b1e17249aa7d86c20b72b1e105
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-11-29 22:56:05 -0500

    grid ready to work

--- src/view/App.java
+++ src/view/App.java
@@ -13,15 +13,15 @@ public class App extends Application {
   private static int WIDTH = 1000;
   private static int HEIGHT = 800;
 
-  private static int WIDTH_GRID = 1000;
-  private static int HEIGHT_GRID  = 800;
+  private static int WIDTH_GRID = 400;
+  private static int HEIGHT_GRID  = 450;
   @Override
   public void start(Stage primaryStage) {
     this.stage = primaryStage;
     stage.setTitle("SafeDrive");
 
     initRootLayout();
-    this.initGrid(100, 100);
+    this.initGrid(80, 80);
   }
 
   public static void main(String[] args) {
@@ -53,7 +53,7 @@ public class App extends Application {
       Controller controller = loader.getController();
       //GRID
       PanView panView = new PanView(y, x, width, height);
-      panView.setLayout(108, 68);
+      panView.setLayout(33, 52);
       controller.setPanView(panView);
 
 //      LayoutX="108.0" layoutY="68.0" prefHeight="816.0" prefWidth="862.0"
