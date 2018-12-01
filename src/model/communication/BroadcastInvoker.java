0c6a2a714bc91070cff0f18fa1b35866efc87413
Irindul <mathieu.regnard2@gmail.com>
2018-12-01 14:28:25 -0500

    Broadcast get receivers from messages

--- src/model/communication/BroadcastInvoker.java
+++ src/model/communication/BroadcastInvoker.java
@@ -1,23 +1,16 @@
 package model.communication;
 
-import java.util.List;
-
 public class BroadcastInvoker implements Invoker {
 
-  private List<Receiver> receivers;
   private DialogInvoker dialogInvoker;
 
   public BroadcastInvoker(DialogInvoker dialogInvoker) {
     this.dialogInvoker = dialogInvoker;
   }
 
-  public void setReceivers(List<Receiver> receivers) {
-    this.receivers = receivers;
-  }
-
   @Override
   public void invoke(Command command) {
-    receivers.forEach(receiver -> {
+    command.getReceivers().forEach(receiver -> {
       dialogInvoker.setReceiver(receiver);
       dialogInvoker.invoke(command);
     });
