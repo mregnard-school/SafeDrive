bf92f8e610354c435c24c42edd4af4555dc5fe96
Irindul <mathieu.regnard2@gmail.com>
2018-12-01 14:48:06 -0500

    Changed vehicle construction

--- src/model/environment/Direction.java
+++ src/model/environment/Direction.java
@@ -4,25 +4,25 @@ import java.awt.Point;
 
 public enum Direction {
   NORTH {
-    public Point position(Point point) {
+    public Point next(Point point) {
       return new Point(point.x, point.y -1);
     }
   },
   SOUTH {
-    public Point position(Point point) {
+    public Point next(Point point) {
       return new Point(point.x, point.y + 1);
     }
   },
   EAST {
-    public Point position(Point point) {
+    public Point next(Point point) {
       return new Point(point.x + 1, point.y);
     }
   },
   WEST {
-    public Point position(Point point) {
+    public Point next(Point point) {
       return new Point(point.x -1, point.y);
     }
   };
 
-  public abstract Point position(Point point);
+  public abstract Point next(Point point);
 }
